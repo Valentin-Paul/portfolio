b96258db0fed5c74f440b7d24c34505af518ffae
Valentin Paul <valentin@vamptech.de>
2022-12-01 02:13:28 +0100

    Added all animations to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactP5Wrapper } from 'react-p5-wrapper';
+import sketch from "./components/DrawLines"
+import randomLines from './components/RandomLines';
+import MovingSquares from './components/MovingSquares';
+import AnimationTennis from './components/AnimationTennis';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className='header'>
+     <h1 className='two'>Valentin Paul</h1>
+     <ReactP5Wrapper sketch={randomLines}></ReactP5Wrapper>
+     </div>
+    <ReactP5Wrapper sketch={AnimationTennis}/>
+
+
+     <div>
+      <h2>Full-Stack Developer </h2>
+      <h2>based in Vienna, Austria.</h2>
+     </div>
+
+      <div className='moving-squares'>
+    <ReactP5Wrapper sketch={MovingSquares}/>
+    </div>
+
+     <div className='circle-drawing'>
+     <ReactP5Wrapper sketch={sketch}/>
+     </div>
+   
+   
+    
+    
+    
+   
       </header>
     </div>
   );
