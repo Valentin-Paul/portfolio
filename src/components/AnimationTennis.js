615d82713391e52bdcf11fbd8d29e8bb52c3995b
Valentin Paul <valentin@vamptech.de>
2022-12-02 20:57:05 +0100

    implemented AnimationTennis with spinning 3d model

---
+++ src/components/AnimationTennis.js
@@ -0,0 +1,46 @@
+import tennis from "./tennisball.obj"
+
+function AnimationTennis(p){
+
+    let width = window.innerWidth / 4.1
+let ani;
+let rot = 0
+let scaleNum = 0.5
+
+    p.preload = () =>{
+        ani = p.loadModel(tennis)
+    }
+
+    p.setup = () =>{
+        p.createCanvas(width, 250, p.WEBGL)
+    }
+
+
+    p.draw = ()=>{
+        rot += 0.1
+        p.background(0, 0, 0)
+        // p.rotateZ(p.frameCount * 0.01);
+        // p.rotateX(p.frameCount * 0.01);
+        p. translate(0, 50, 0);
+        p. rotateX(3);
+        // p. rotateZ(p.frameCount * 0.01);
+        // p.rotateX(p.frameCount * 0.01);
+        p.fill(255, 255, 255)
+        p.rotateY(p.frameCount * 0.015);
+        p.strokeWeight(0.2)
+        p.scale(scaleNum)
+        p.model(ani)
+
+
+
+        if ((p.mouseX < width) && (p.mouseX > 0) &&
+        (p.mouseY > 0) && (p.mouseY < 250)) {
+            scaleNum = 0.58
+        }
+        else{
+            scaleNum = 0.5
+        }
+    }
+}
+
+export default AnimationTennis
