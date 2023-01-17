b4cf763e3ece6c7f1a2d8a5d63a41f8321184561
Valentin Paul <valentin@vamptech.de>
2023-01-17 18:37:01 +0100

    changed 3d nimation of hotdog bun to lightbulb

--- src/components/AnimationGame.js
+++ src/components/AnimationGame.js
@@ -1,4 +1,4 @@
-import gameboy from "./gameboy2.obj"
+import lightbulb from "./lightbulb.obj"
 
 function AnimationGame(p){
 
@@ -8,7 +8,7 @@ let rot = 0
 let scaleNum
 
     p.preload = () =>{
-        ani = p.loadModel(gameboy)
+        ani = p.loadModel(lightbulb)
     }
 
     p.setup = () =>{
@@ -21,23 +21,24 @@ let scaleNum
         p.background(0, 0, 0)
         // p.rotateZ(p.frameCount * 0.01);
         // p.rotateX(p.frameCount * 0.01);
-        p. translate(0, -5, 120);
-        p. rotateX(14);
-        p.rotateY(3.15)
+        p. translate(0, 100, -80);
+        p. rotateX(0);
+        p.rotateY(p.frameCount * -0.018)
+        p.rotateZ(3.3)
         // p.rotateX(p.frameCount * 0.01);
         p.fill(255, 255, 255)
-        p.rotateZ(p.frameCount * -0.018);
-        p.strokeWeight(0.05)
+        // p.rotateZ(p.frameCount * -0.018);
+        p.strokeWeight(0.35)
         p.fill(255, 255, 255)
         p.scale(scaleNum)
         p.model(ani)
 
         if ((p.mouseX < width) && (p.mouseX > 0) &&
         (p.mouseY > 0) && (p.mouseY < 250)) {
-            scaleNum = 38
+            scaleNum = 26
         }
         else{
-            scaleNum = 32
+            scaleNum = 23
         }
        
       
